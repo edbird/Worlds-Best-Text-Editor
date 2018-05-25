a669926e4a8d74804cf14e5fa82f9d694d1476fa
edbird <edbird@users.noreply.github.com>
2018-05-25 13:46:24 +0100

    moved print_char_texture function to seperate file to avoid repeating in every class which uses it

---
+++ include/Functions.hpp
@@ -0,0 +1,5 @@
+
+void print_char_texture(SDL_Renderer *const renderer,
+                        SDL_Texture* const texture,
+                        const SDL_Rect& src_rect,
+                        SDL_Rect& dst_rect);
